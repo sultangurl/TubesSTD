5bde0cb775f881d2731c4dcc28398921c64c0206
sultangurl <arvinda.ds20@gmail.com>
2020-04-27 17:02:37 +0700

    Revert "Revert "Revert "Initial commit"""
    This reverts commit 6e5b79839c9acdebbd08476d5e0ab5e0aed34a87.

--- novel.cpp
+++ novel.cpp
@@ -13,68 +13,19 @@ adrN createNewN(string nama) {
 }
 
 void addN(ListN &L, adrN P) {
-    adrN f = findN(L, info(P));
-    if (f != NULL) {
-        cout<<"Novel sudah terdaftar."<<endl;
-    } else {
-        if (first(L) != NULL) {
-            adrN Q = first(L);
-            while(next(Q) != NULL) {
-                Q = next(Q);
-            }
-            next(Q) = P;
-            next(P) = NULL;
-        } else {
-            first(L) = P;
-            next(P) = NULL;
+    if (first(L) != NULL) {
+        adrN Q = first(L);
+        while(next(Q) != NULL) {
+            Q = next(Q);
         }
-    }
-}
-
-void deleteN(ListN &L, adrN P) {
-    adrN prec;
-    if (P != NULL) {
-        if (P == first(L)) {
-            deleteFirstN(L, P);
-       } else if (next(P) == NULL) {
-           deleteLastN(L);
-       } else {
-           prec = first(L);
-           while (next(prec) != P) {
-                prec = next(prec);
-           }
-           deleteAfterN(L, prec);
-       }
+        next(Q) = P;
+        next(P) = NULL;
     } else {
-        cout<<"List kosong."<<endl;
+        first(L) = P;
+        next(P) = NULL;
     }
 }
 
-void deleteFirstN(ListN &L, adrN &P) {
-    first(L) = next(P);
-    next(P) = NULL;
-}
-
-void deleteLastN(ListN &L) {
-    adrN P = first(L);
-    while (next(next(P)) != NULL) {
-        P = next(P);
-    }
-    adrN Q = next(P);
-    next(Q) = NULL;
-    next(P) = NULL;
-}
-
-void deleteAfterN(ListN &L, adrN prec) {
-    adrN P = first(L);
-    while (next(P) != NULL && info(P) != info(prec)) {
-        P = next(P);
-    }
-    adrN Q = next(P);
-    next(P) = next(Q);
-    next(Q) = NULL;
-
-}
 
 adrN findN(ListN L, string judul) {
     if (first(L) != NULL) {
@@ -92,12 +43,9 @@ adrN findN(ListN L, string judul) {
 }
 
 void showN(ListN L) {
-    int i = 0;
     adrN P = first(L);
-    cout<<"List novel yang terdaftar:"<<endl;
     while (P != NULL) {
-        i++;
-        cout<<i<<". "<<info(P)<<endl;
+        cout << ">>" << info(P) << endl;
         P = next(P);
     }
 }
